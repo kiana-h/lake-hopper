8159e7a1ee0adfd4f55a15dfe88ea6b9fe3ebfe3
Kiana H <s.kiana.hosseini@gmail.com>
2020-12-25 16:46:04 -0800

    Add about page

---
+++ frontend/components/about/about.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import marked from "marked";
+import MenuItem from "@material-ui/core/MenuItem";
+import style from "./style.scss";
+import theme from "../theme/theme";
+import readme from "../../../README.md";
+
+const About = () => {
+  return (
+    <div
+      className={style.markdownWrapper}
+      dangerouslySetInnerHTML={{ __html: marked(readme.slice(78)) }}
+    ></div>
+  );
+};
+
+export default About;
